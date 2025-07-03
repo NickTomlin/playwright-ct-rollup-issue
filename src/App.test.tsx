f59b0e25dd7e27ee5c266ef17523f27d1ffe29ba
Nick Tomlin <NickTomlin@users.noreply.github.com>
2025-07-03 11:26:04 -0500

    chore: Update expectation

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,10 +4,5 @@ import App from './App';
 test('App component renders', async ({ mount }) => {
   const component = await mount(<App />);
   await expect(component).toContainText('TypeScript Import Test App');
-});
-
-test('App component has editor container', async ({ mount }) => {
-  const component = await mount(<App />);
-  const editorContainer = component.locator('#editor-worker');
-  await expect(editorContainer).toBeVisible();
+  await expect(component).toContainText('NamespaceModuleExcludes')
 });
