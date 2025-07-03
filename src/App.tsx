7134e184498af02a48d6f41d5f376327acd533dc
Nick Tomlin <NickTomlin@users.noreply.github.com>
2025-07-03 11:28:14 -0500

    chore: More

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
+// comment this out to have npm run ct pass
 import ts from "typescript"
-
-console.log('runtime', ts.SymbolFlags)
 
 const App: React.FC = () => {
   return (
     <div>
       <h1>TypeScript Import Test App</h1>
+      {/* comment tout this to have npm run CT pass  */}
       Ts symbol flag[0]: {ts.SymbolFlags[0]}
       <div id="editor-worker"></div>
     </div>
